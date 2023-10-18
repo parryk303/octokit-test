877b4a3396bb64defb83ae9a5667cd864f473a4a
Kyle Parry <66541350+parryk303@users.noreply.github.com>
2023-10-18 15:25:02 -0600

    Update 1234567/main.tf

--- 1234567/main.tf
+++ 1234567/main.tf
@@ -3,10 +3,10 @@ module "cloud_relay" {
 source = "../../module/main"
 name = "t"
 env = var.env
-cidr = [[&#39;10.1.0.0/26&#39;]]
+cidr = [&#34;10.1.0.0/26&#34;]
 ring_central_subnet = ["10.1.0.0/28"]
 
-vpn_connection_static_routes_destinations = [[&#39;10.1.1.19&#39;]]
+vpn_connection_static_routes_destinations = [&#34;10.1.1.19&#34;]
 customer_gateway_ip_address = ["200.1.2.3"]
 vpn_connection_tunnel1_phase1_encryption_algorithms = ["AES256"]
 vpn_connection_tunnel1_phase1_integrity_algorithms = ["SHA2-512"]
