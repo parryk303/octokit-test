375c8e9634deadd0b94d0d967c78b1407652a6a3
Kyle Parry <66541350+parryk303@users.noreply.github.com>
2023-10-20 09:18:52 -0600

    Update 123-65320b773e1d32f95961cce8/main.tf

--- 123-65320b773e1d32f95961cce8/main.tf
+++ 123-65320b773e1d32f95961cce8/main.tf
@@ -6,7 +6,7 @@ env = var.env
 cidr = ["10.1.0.0/26"]
 ring_central_subnet = ["10.1.0.0/28"]
 
-vpn_connection_static_routes_destinations = ["10.1.1.19", "10.1.1.19", "10.1.1.19"]
+vpn_connection_static_routes_destinations = ["10.1.1.19"]
 customer_gateway_ip_address = ["10.1.1.19"]
 vpn_connection_tunnel1_phase1_encryption_algorithms = ["AES128"]
 vpn_connection_tunnel1_phase1_integrity_algorithms = ["SHA1"]
