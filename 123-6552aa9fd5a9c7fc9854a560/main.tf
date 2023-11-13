1e62e114763bb1009bf9aabb580ce5a2666649c9
Kyle Parry <66541350+parryk303@users.noreply.github.com>
2023-11-13 16:00:55 -0700

    Update 123-6552aa9fd5a9c7fc9854a560/main.tf

---
+++ 123-6552aa9fd5a9c7fc9854a560/main.tf
@@ -0,0 +1,51 @@
+
+module "cloud_relay" {
+source = "../../module/main"
+name = "123-6552aa9fd5a9c7fc9854a560"
+env = var.env
+cidr = "Input is not an array"
+ring_central_subnet = []
+relay_package_location = "lambdas_code/relay-lambda-code.zip"
+authorizer_package_location = "lambdas_code/authorizer-lambda-code.zip"
+enable_vpn_build = false
+}
+
+
+output "project_name" {
+value = module.cloud_relay.name
+}
+
+output "project_env" {
+value = var.env
+}
+
+output "main_regions" {
+value = module.cloud_relay.main_regions
+}
+
+output "backup_regions" {
+value = module.cloud_relay.backup_regions
+}
+
+output "API_url" {
+value = module.cloud_relay.API_url
+}
+
+output "backup_API_url" {
+value = module.cloud_relay.backup_API_url
+}
+
+output "apisecret" {
+value = module.cloud_relay.apisecret
+sensitive = true
+}
+
+output "api_key" {
+value = module.cloud_relay.api_key
+sensitive = true
+}
+
+output "backup_api_key" {
+value = module.cloud_relay.backup_api_key
+sensitive = true
+}
