2c83d398f6f86a1fe373a936428ac4205942d94f
Kyle Parry <66541350+parryk303@users.noreply.github.com>
2023-11-22 21:10:58 -0700

    Update terraform/projects/007: elegant-goldberg/main.tf

---
+++ terraform/projects/007: elegant-goldberg/main.tf
@@ -0,0 +1,16 @@
+
+    module "cloud_relay" {
+      source              = "../../module/main"
+      name                = "007: elegant-goldberg"
+      env                 = var.env
+      cidr                = "Input is not an array"
+      private_subnets     = Input is not an array
+      public_subnets      = [""]
+
+      relay_package_location      = "lambdas_code/relay-lambda-code.zip"
+      authorizer_package_location = "lambdas_code/authorizer-lambda-code.zip"
+
+      enable_vpn_build = false
+
+}
+
