59d95af522b02ca40ef6de247f186c421fd2056e
Kyle Parry <66541350+parryk303@users.noreply.github.com>
2023-11-27 14:05:56 -0700

    Update terraform/projects/1234_infallible-keller/main.tf

---
+++ terraform/projects/1234_infallible-keller/main.tf
@@ -0,0 +1,15 @@
+
+  module "cloud_relay" {
+    source              = "../../module/main"
+    name                = "infallible-keller"
+    env                 = var.env
+    cidr                = "172.31.0.0/28"
+    private_subnets     = Input is not an array
+    public_subnets      = [""]
+
+    relay_package_location      = "lambdas_code/relay-lambda-code.zip"
+    authorizer_package_location = "lambdas_code/authorizer-lambda-code.zip"
+
+    enable_vpn_build = false
+}
+
