06aade7a8f2e47c3282eb8d71c624dcb3b8f59fd
Kyle Parry <66541350+parryk303@users.noreply.github.com>
2023-11-27 12:48:20 -0700

    Update terraform/projects/12_mystifying-curran/main.tf

--- terraform/projects/12_mystifying-curran/main.tf
+++ terraform/projects/12_mystifying-curran/main.tf
@@ -3,9 +3,9 @@
     source              = "../../module/main"
     name                = "12_mystifying-curran"
     env                 = var.env
-    cidr                = "Input is not an array"
-    private_subnets     = Input is not an array
-    public_subnets      = [""]
+    cidr                = "172.31.0.0/28"
+    private_subnets     = ["172.31.0.0/28", "172.31.0.16/28"]
+    public_subnets      = ["172.31.0.32/27"]
 
     relay_package_location      = "lambdas_code/relay-lambda-code.zip"
     authorizer_package_location = "lambdas_code/authorizer-lambda-code.zip"
