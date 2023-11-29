da0d41173df88027b9474c01f4be15e0d2379ca8
Kyle Parry <66541350+parryk303@users.noreply.github.com>
2023-11-29 10:45:53 -0700

    Update terraform/projects/123_VIGOROUSLAMPORT/main.tf

---
+++ terraform/projects/123_VIGOROUSLAMPORT/main.tf
@@ -0,0 +1,15 @@
+
+  module "cloud_relay" {
+    source              = "../../module/main"
+    name                = "VIGOROUSLAMPORT"
+    env                 = var.env
+    cidr                = "172.31.0.0/28"
+    private_subnets     = ["172.31.0.0/28", "172.31.0.16/28"]
+    public_subnets      = ["172.31.0.32/27"]
+
+    relay_package_location      = "lambdas_code/relay-lambda-code.zip"
+    authorizer_package_location = "lambdas_code/authorizer-lambda-code.zip"
+
+    enable_vpn_build = false
+}
+
