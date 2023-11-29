a225e3f879cb8aeb3a48f555258b62a72c62a779
Kyle Parry <66541350+parryk303@users.noreply.github.com>
2023-11-29 11:51:46 -0700

    Update terraform/projects/123_RELAXEDARYABHATA/main.tf

--- terraform/projects/123_RELAXEDARYABHATA/main.tf
+++ terraform/projects/123_RELAXEDARYABHATA/main.tf
@@ -4,8 +4,8 @@
     name                = "RELAXEDARYABHATA"
     env                 = var.env
     cidr                = "172.31.0.0/28"
-    private_subnets     = ["172.31.0.0/28", "172.31.0.16/28"]
-    public_subnets      = ["172.31.0.32/27"]
+    private_subnets     = ["10.192.154.0/28", "10.192.154.16/28"]
+    public_subnets      = ["10.192.154.0.32/27"]
 
     relay_package_location      = "lambdas_code/relay-lambda-code.zip"
     authorizer_package_location = "lambdas_code/authorizer-lambda-code.zip"
