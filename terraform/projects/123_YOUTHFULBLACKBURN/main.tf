5dfe36d7cd5461d88d3625d4364374a9fe2b3d2a
Kyle Parry <66541350+parryk303@users.noreply.github.com>
2023-11-30 13:14:23 -0700

    Update terraform/projects/123_YOUTHFULBLACKBURN/main.tf

--- terraform/projects/123_YOUTHFULBLACKBURN/main.tf
+++ terraform/projects/123_YOUTHFULBLACKBURN/main.tf
@@ -4,8 +4,8 @@
     name                = "YOUTHFULBLACKBURN"
     env                 = var.env
     cidr                = "172.31.0.0/28"
-    private_subnets     = ["192.168.0.0/28", "192.168.0.16/28"]
-    public_subnets      = ["192.168.0.32/27"]
+    private_subnets     = ["172.31.0.0/28", "172.31.0.16/28"]
+    public_subnets      = ["172.31.0.32/27"]
 
     relay_package_location      = "lambdas_code/relay-lambda-code.zip"
     authorizer_package_location = "lambdas_code/authorizer-lambda-code.zip"
